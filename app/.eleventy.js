3ea71dfc32e3eb60fc60faea1d0010e4e4857b3e
Chet Hardin <chet.hardin@gmail.com>
2021-08-16 13:09:52 -0500

    working

--- app/.eleventy.js
+++ app/.eleventy.js
@@ -1,4 +1,10 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", (contents) =>
+        yaml.safeLoad(contents)
+    );
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy('admin');
     eleventyConfig.addPassthroughCopy("js");
