c336dfc91be0a49bdd368fbdf1191dc8a1566aab
Chet Hardin <chet.hardin@gmail.com>
2021-08-16 10:22:55 -0500

    seamless

--- app/.eleventy.js
+++ app/.eleventy.js
@@ -1,15 +1,12 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("images");
-	eleventyConfig.addPassthroughCopy('admin');
-	eleventyConfig.addPassthroughCopy("js");
-	eleventyConfig.addPassthroughCopy("css");
-	eleventyConfig.addPassthroughCopy('scss');
-	eleventyConfig.addPassthroughCopy({
-		"node_modules/seamless-scroll-polyfill/dist/es5/seamless.js": "js/seamless.js"
-	});
-	eleventyConfig.addFilter('dump', obj => {
-		return util.inspect(obj)
-	});
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy('scss');
+    eleventyConfig.addFilter('dump', obj => {
+        return util.inspect(obj)
+    });
 };
 
 const util = require('util')
