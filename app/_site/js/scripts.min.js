199d4d383d6797823c869a05c308d5ec93d3f565
Chet Hardin <chet.hardin@gmail.com>
2021-08-16 13:23:53 -0500

    updates

--- app/_site/js/scripts.min.js
+++ app/_site/js/scripts.min.js
@@ -1,3 +1,89 @@
-let c = 0;
-setInterval(function () { w() }, 30);
-function w() { c += 0.001; document.querySelector(".js-kitty").style.transform = 'scale(' + c + ')' }
+seamless.polyfill();
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.querySelector(".js-form");
+    let h2 = document.querySelector(".h2--modal");
+    let formData = new FormData(myForm)
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(
+        () => {
+            h2.innerText = 'You\'re on the list!'
+            myForm.innerHTML = '<p class="white confirmation">Thank you for your submission. We will be in touch soon!</p>';
+            setTimeout(function () { closeModal() }, 2000);
+        }
+    ).catch((error) =>
+        alert(error)
+    )
+}
+document.querySelector(".js-form").addEventListener("submit", handleSubmit);
+
+// Smooth scroll
+document.querySelector(".js-arrow-down").addEventListener("click", function (e) {
+    e.preventDefault();
+    const offsetTop = document.querySelector('#js-scroll').offsetTop;
+    seamless.elementScrollIntoView(
+        document.querySelector('#js-scroll'), {
+        top: offsetTop,
+        behavior: "smooth"
+    });
+});
+
+const options = {
+    threshold: 0.15
+};
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fadeInUp');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+const observer = new IntersectionObserver(callback, options);
+document.querySelectorAll('.js-animated').forEach(animation => {
+    observer.observe(animation);
+});
+
+const modal = document.querySelector('.js-modal');
+const overlay = document.querySelector('.js-overlay');
+const close = document.querySelector('.js-close');
+const heroModalLaunch = document.querySelector('.js-hero-modal-launch');
+const emailHolder = document.querySelector('.js-email-holder');
+
+document.querySelectorAll('.js-modal-launch').forEach(item => {
+    item.addEventListener('click', event => {
+        openModal()
+    })
+})
+
+function heroModal() {
+    var email = emailHolder.value;
+    document.getElementById('email').value = email;
+    openModal()
+}
+
+overlay.onclick = function () {
+    closeModal()
+}
+
+close.onclick = function () {
+    closeModal()
+}
+
+function openModal() {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+    close.classList.add("active");
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+    close.classList.remove("active");
+    document.body.style.overflow = 'auto';
+}
